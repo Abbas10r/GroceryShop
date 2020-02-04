52b9ed7d6aedbf15b7d8f815285dab064d9cb3c3
Abbas Azisov <abbastktl@gmail.com>
2020-02-05 01:11:51 +0600

    remade with method and tryCatch

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -8,6 +8,23 @@ namespace Task5
 {
     class Program
     {
+        static void adding_product(List<Product> list)
+        {
+            WriteLine("Type a name of product: ");
+            string name = ReadLine();
+            float cost = 0;
+            WriteLine("Type a cost of product: ");
+            try
+            {
+                cost = float.Parse(ReadLine());
+            }
+            catch (System.FormatException)
+            {
+                WriteLine("You've pressed a wrong value.");
+            }
+            Product p1 = new Product(name, cost);
+            list.Add(p1);
+        }
         static void Main(string[] args)
         {
             bool turnon=true;
@@ -15,29 +32,42 @@ namespace Task5
             WriteLine("Welcome to Abbas's Grocery Shop!");           
             while (turnon)
             {
-                WriteLine("Press 1 to add groceries to shop.\n" +
-                "Press 2 to see a list of products.\n" +
-                "Press 3 to sort a list.\n" +
+                WriteLine("Press 1 to add product to shop.\n" +
+                    "Press 2 to remove a product\n"+
+                "Press 3 to see a list of products.\n" +
+                "Press 4 to sort a list.\n" +
                 "Press 0 to close a program.");
-                int button = ToInt32(ReadLine());              
+                int button = 0;
+                try
+                {
+                    button = ToInt32(ReadLine());
+                }                  
+                catch(System.FormatException)
+                {
+                    WriteLine("You've pressed a wrong value.");
+                }            
                 switch (button)
                 {
                     case 0:
                         turnon = false;
                         break;
                     case 1:
-                        WriteLine("Type a name of product: ");
-                        string name = ReadLine();
-                        WriteLine("Type a cost of product: ");
-                        float cost = float.Parse(ReadLine());
-                        Product p1 = new Product(name,cost);
-                        products_list.Add(p1);
+                        adding_product(products_list);
                         break;
                     case 2:
-                        foreach (Product item in products_list)
-                            Console.WriteLine(item.product_name + " - "+item.product_cost);
+                        WriteLine("Enter index of a product: ");
+                        int index = ToInt32(ReadLine());
+                        products_list.RemoveAt(index);
                         break;
                     case 3:
+                        int counter = 0;
+                        foreach (Product item in products_list)
+                        {                         
+                            Console.WriteLine("index - "+counter + " " + item.product_name + " - " + item.product_cost);
+                            counter++;
+                        }
+                        break;
+                    case 4:
                         var byCost = from u in products_list
                                      orderby u.product_cost descending //from higher to lower. Can remove "descending" for reverse sort
                                      select u;
